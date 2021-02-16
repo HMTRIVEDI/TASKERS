b67751bf71c86e4e5b4950e4c5b39d2f7903c801
HMTRIVEDI <hirenkumar.t@yahoo.com>
2021-02-16 12:13:58 +0000

    adding cart app

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Show_cart, name='Show_cart')
+    path('', views.show_cart, name='show_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
+def show_cart(request):
 
-def Show_cart(request):
     return render(request, 'cart/cart.html')
