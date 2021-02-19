b21041bc31aa4665eade11f28558b900c442fc14
HMTRIVEDI <hirenkumar.t@yahoo.com>
2021-02-19 10:29:52 +0000

    typo fixed in booking page

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,27 +1,33 @@
 from decimal import Decimal
 from django.conf import settings
+from service.models import Tasker
+from django.shortcuts import get_object_or_404
 
 
 def cart_contents(request):
 
-    cart_service = []
+    cart_item = []
     cart = request.session.get('cart', {})
     total = 0
-    service_hours = 0
-    visiting_count = Decimal(settings.STANDARD_SESSION)
     service_charge = total * Decimal(
         settings.STANDARD_SERVICE_CHARGE / 100)
     grand_total = total + service_charge
-    
+
+    ##for item_id, hours in cart.item():
+    ##  tasker = get_object_or_404(Tasker, pk=item_id)
+    ## total = hours * tasker.price
+    ## cart_item.append({
+    ## 'item_id': item_id,
+    ## 'hours': hours,
+    ## 'tasker': tasker,
+    ##  })
 
     context = {
-        'cart_service': cart_service,
+        'cart_item': cart_item,
         'total': total,
-        'service_hours': service_hours,
         'service_charge': service_charge,
         'grand_total': grand_total,
-        'visiting_count': visiting_count,
-        'cart' : cart,
+        'cart': cart,
     }
 
     return context
