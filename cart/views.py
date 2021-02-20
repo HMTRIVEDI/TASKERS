a263c1801bb5f9c7fb13ecbd8e7852026388c7fa
HMTRIVEDI <hirenkumar.t@yahoo.com>
2021-02-20 12:31:17 +0000

    bug fix in add to cart functon

--- cart/views.py
+++ cart/views.py
@@ -13,16 +13,12 @@ def add_booking(request, tasker_id):
     hours = int(request.POST.get('hours'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-    
-    print(request.POST.get('date'))
-    print(request.POST.get('time'))
-    print(request.POST.get('hours'))
-    print(request.POST.get('redirect_url'))
-    
 
     if tasker_id in list(cart.keys()):
         cart[tasker_id] = date, time, hours
-        
+    else:
+        cart[tasker_id] = date, time, hours
+
     request.session['cart'] = cart
     print(request.session.get('cart', {}))
     return redirect(redirect_url)
