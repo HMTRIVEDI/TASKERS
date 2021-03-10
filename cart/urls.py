95c8811bdc1de71a721be05b852a317f046b2be4
HMTRIVEDI <hirenkumar.t@yahoo.com>
2021-03-10 06:48:51 +0000

    update and delete function done for cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.show_cart, name='show_cart'),
     path('addbooking/<item_id>/', views.add_booking, name='add_booking'),
+    path('delete/<item_id>/', views.delete_booking, name='delete_booking'),
+    path('update/<item_id>/', views.update_booking, name='update_booking'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def show_cart(request):
@@ -21,3 +21,38 @@ def add_booking(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def delete_booking(request, item_id):
+
+    cart = request.session.get('cart', {})
+    if item_id in cart:
+        cart.pop(item_id)
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    print(cart)
+    return redirect(reverse('show_cart'))
+
+
+def update_booking(request, item_id):
+
+    date = request.POST.get('date')
+    time = request.POST.get('time')
+    hours = int(request.POST.get('hours'))
+    cart = request.session.get('cart', {})
+
+    if hours > 8:
+        print(" no more then 8 hours ")
+    elif hours > 0 and hours < 9:
+        if item_id in list(cart.keys()):
+            cart[item_id] = date, time, hours
+        else:
+            cart[item_id] = date, time, hours
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    print(cart)
+    return redirect(reverse('show_cart'))
